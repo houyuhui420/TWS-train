00ef1ae8ed0328721de2a089eb76033b8eb62850
houyuhui <963655267@qq.com>
2017-08-08 22:22:09 +0800

    正整数分解质因数

--- 08.js
+++ 08.js
@@ -19,10 +19,9 @@ return originNum + "=" + arry.join("");
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin,process.stdout);
 
-// question方法
+
 rl.question('请输入一个正整数： ',function(answer){
   console.log(count(answer));
-  //不加close，则不会结束
   rl.close();
   process.exit(0);
-}); 
+});
