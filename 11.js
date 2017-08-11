574e137d76a6ce3161b64f87438c23d0f5bd4e7f
houyuhui <963655267@qq.com>
2017-08-11 14:43:00 +0800

    删除字符串中特定的字符串

--- 11.js
+++ 11.js
@@ -1,5 +1,11 @@
-var str = "1->2->3->3->4->5->3";
+//var str = "1->2->3->3->4->5->3";
 
-var result = str.replace(/->3/g,"");
+//var result = str.replace(/->3/g,"");
 
-console.log(result);
+function result (str,val){
+  let reg = new RegExp("->" + val,"g");
+  let strNew = str.replace(reg,"");
+  return strNew;
+}
+
+console.log(result("1->2->3->3->4->5->3",3));
