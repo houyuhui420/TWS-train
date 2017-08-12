42d2fdd640aceb015ff8b749bc5f90f6a1acf533
houyuhui <963655267@qq.com>
2017-08-12 21:28:23 +0800

    一个三位数，其各位数字立方和等于该数本身

---
+++ 12.js
@@ -0,0 +1,16 @@
+function find(){
+  let result = 0;
+  for(let i = 99;i<1000;i++){
+    let array = i.toString().split("").map(value => ~~value);
+    let count = 0;
+    array.forEach((value) => {
+      count += value*value*value;
+    });
+    if(count === i){
+      result = i;
+      console.log(i);
+    }
+  }
+}
+
+find();
