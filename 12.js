919c579f10e7482a1f3cffcf2b45da8cff9d64aa
houyuhui <963655267@qq.com>
2017-08-13 20:07:18 +0800

    打印出所有三位数，其各位数字立方和等于该数本身

--- 12.js
+++ 12.js
@@ -1,12 +1,16 @@
-function findNum(array){
-  let arrayNew = array.sort();
-  let num = 0;
-  if(arrayNew.length%2 === 0){
-    num = (arrayNew[arrayNew.length/2 - 1] + arrayNew[arrayNew.length/2])/2;
-  }else if(arrayNew.length%2 !== 0){
-    num = arrayNew[Math.floor(arrayNew.length/2)];
+function find(){
+  let result = 0;
+  for(let i = 99;i<1000;i++){
+    let array = i.toString().split("").map(value => ~~value);
+    let count = 0;
+    array.forEach((value) => {
+      count += value*value*value
+    });
+    if(count === i){
+      result = i;
+      console.log(i);
+    }
   }
-  console.log(num);
 }
- findNum([1,3,6,4,7]);
-findNum([1,6,4,8,5,9]);
+
+find();
